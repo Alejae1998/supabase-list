f8a28a3899a19475d99b44b5b12f46242f88ef76
Alejae1998 <alejagr1998@gmail.com>
2022-05-11 14:50:09 -0700

    all functions working

---
+++ animal-detail/data.js
@@ -0,0 +1,29 @@
+export const animals = [
+    {
+        id: '1',
+        name: 'Aby',
+        Image: '../assets/redpanda.svg',
+        type: 'redpanda',
+        says: 'Have a pandastic day',
+        top: '100px',
+        left: '50%',
+    },
+    {
+        id: '2',
+        name: 'Harry',
+        Image: '../assets/otter.svg',
+        type: 'otter',
+        says: 'You are otterly amazing!',
+        top: '315px',
+        left: '50%',
+    },
+    {
+        id: '3',
+        name: 'Buttercup',
+        Image: '../assets/horse.svg',
+        type: 'horse',
+        says: 'Someday I will be a unicorn',
+        top: '370px',
+        left: '65%',
+    },
+];
